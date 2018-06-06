c56ccb0e62cbe3b4169875c0822abfcc23af9950
jjing2011 <jjing2011@gmail.com>
2018-06-06 09:46:55 +0800

    6/5 update some exec

---
+++ file_reader.py
@@ -0,0 +1,13 @@
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.strip())
+
+with open('pi_digits.txt') as file_object:
+    for line in file_object:
+        print(line.strip())
+
+with open('pi_digits.txt') as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.strip())
---
+++ test_name_function.py
@@ -0,0 +1,15 @@
+import unittest
+from name_function import get_formatted_name
+
+class NameTestCase(unittest.TestCase):
+
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_name('janis', 'joplin', 'jr.')
+        self.assertEqual(formatted_name, 'Janis Jr. Joplin')
+    
+    def test_first_last_name(self):
+        
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, "Janis Joplin")
+
+unittest.main()
